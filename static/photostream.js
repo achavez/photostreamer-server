59285f732347dac0c96529bb2206fea320c0d808
Andrew Chavez <adchavez@gmail.com>
2014-05-30 17:52:31 -0600

    Fix for buggy behavior when for first streamed photo

--- static/photostream.js
+++ static/photostream.js
@@ -158,6 +158,12 @@ var PhotostreamView = Backbone.View.extend({
 		this.render();
 
 		this.collection.on("add", function(photo) {
+			// If this is the first photo to be streamed,
+			// remove the empty text
+			if(this.collection.length == 1) {
+				this.$el.html('');
+			}
+
 			var el = this.renderSingle(photo);
 			this.$el.prepend(el);
 			fixHeights(true);
@@ -192,10 +198,6 @@ var DownloadsView = Backbone.View.extend({
 		this.setElement(this.el);
 		this.empty = this.$el.html();
 		this.render();
-
-		this.collection.on("add", function(download) {
-			this.addSingle(download);
-		}, this);
 
 		this.collection.on("change", function(download) {
 			if(download.hasChanged('full')) {
