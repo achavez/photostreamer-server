329a21654c143842159f5558ca768dbd87645bb2
Andrew Chavez <adchavez@gmail.com>
2014-05-30 20:19:45 -0600

    Trigger reflow when individual photo tiles are re-rendered

--- static/photostream.js
+++ static/photostream.js
@@ -106,6 +106,7 @@ var PhotoView = Backbone.View.extend({
 
 		this.model.on("sync", function() {
 			this.render();
+			fixHeights();
 		}, this);
 	},
 	className: 'photo col-xs-12 col-sm-6 col-md-4 col-lg-3',
