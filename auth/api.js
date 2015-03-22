2ce196d70c91e8b23a34d71d081e60955db586a6
Andrew Chavez <adchavez@gmail.com>
2015-03-22 01:22:32 -0500

    Add key-based authorization to the API

---
+++ auth/api.js
@@ -0,0 +1,31 @@
+var User = require('../models').User,
+    restify = require('express-restify-mongoose');
+
+/*
+ * Middleware for all API requests that looks for a token
+ * parameter and validates it against the list of authorized
+ * users, returning an error if it's invalid.
+ */
+module.exports = function(req, res, next) {
+  // TODO: Skip validation based on header if there's already a
+  // user set meaning (meaning this is probably a Web request
+  // that's already authed)
+
+  var key = req.header('Authorization');
+
+  if(typeof key === 'undefined') {
+    return res.send(400, 'An API key is required.');
+  }
+
+  User.findByKey(key, function(err, user) {
+    if(err) return next(err);
+
+    if(user) {
+      req.user = user;
+      next();
+    }
+    else {
+      res.send(401, 'Invalid API key.');
+    }
+  });
+};
