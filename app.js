bd9f3eae63fbe6177b7c5fda761d94a9dcb5a8be
Andrew Chavez <adchavez@gmail.com>
2015-03-29 15:13:15 -0500

    Handle first user creation

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(session);
 app.use(passwordless.sessionSupport());
 app.use('/login', passwordless.acceptToken({ successRedirect: '/'}));
 
-// Hydrate user date in req.locals
+// Hydrate user data in req.locals
 app.use(auth.hydrate);
 
 // Setup RESTful API with Restify
