aef2228a7802f2760999356aa9e994e8ef3c0494
Andrew Chavez <adchavez@gmail.com>
2015-03-29 13:06:43 -0500

    Add auth to websockets; share session data between Express and Primus

--- auth/index.js
+++ auth/index.js
@@ -1,3 +1,4 @@
 exports.api = require('./api');
 exports.web = require('./web');
 exports.hydrate = require('./hydrate');
+exports.socket = require('./socket');
---
+++ auth/socket.js
@@ -0,0 +1,61 @@
+var session = require('express-session'),
+    MongoStore = require('connect-mongo')(session);
+
+var config = require('../lib/config');
+
+var store = new MongoStore({
+  url: config.dbUrl
+});
+
+/*
+* Auth middleware for Primus sockets. Just checks to see if
+* the user is already authenticated using the Web auth method.
+*
+* Also populates req.session for the passed spark.
+*
+* Based heavily on: https://github.com/primus/primus/tree/master/examples/middleware
+*/
+
+module.exports = function (req, next) {
+  // Get the session ID from the cookies (parsed by earlier
+  // cookie-parser middleware)
+  var sid = req.signedCookies['connect.sid'];
+
+  // If there's no session cookie, move on
+  if (!sid) return next({
+    statusCode: 401
+  });
+
+  // Build the session object
+  req.session = {};
+
+  // Pause the request before retrieving the session
+  req.pause();
+
+  // Lookup the session
+  store.get(sid, function (err, session) {
+    // Resume the request
+    req.resume();
+
+    // Handle errors
+    if (err) {
+      primus.emit('log', 'error', err);
+      return next({
+        statusCode: 500
+      });
+    }
+
+    // Populate the session
+    if (session) req.session = session;
+
+    // Make sure the session is for an authorized user
+    if(!req.session.hasOwnProperty('passwordless')) {
+      return next({
+        statusCode: 401
+      });
+    }
+
+    // If the user is authed, fire up the socket
+    next();
+  });
+};
